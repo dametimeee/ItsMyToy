1f3bf702656c32a5025ecd9724a02aa8a1c0b931
Minsu Jeong <as01100@naver.com>
2023-01-21 17:01:23 +0900

    Update recoil state

---
+++ client/src/recoil/state.js
@@ -0,0 +1,27 @@
+import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist();
+
+export const userState = atom({
+  key: "userState",
+  default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const loginState = atom({
+  key: "loginState",
+  default: false,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const isNaverState = atom({
+  key: "companyState",
+  default: true,
+  effects_UNSTABLE: [persistAtom],
+});
+
+// export const webtoonState = atom({
+//   key: "webtoonState",
+//   default: fetch(`https://korea-webtoon-api.herokuapp.com/?perPage=3000`),
+// });
